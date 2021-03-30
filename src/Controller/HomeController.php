46b04f8134bee7ca03d301d92fbc544ff723a9c8
Olivier Meunier <oli.meunier@gmail.com>
2021-03-30 15:02:40 +0200

    search form with CocktailSearchType

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\CocktailSearchType;
 use App\Repository\CategoryRepository;
 use App\Repository\CocktailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,27 +17,42 @@ class HomeController extends AbstractController
      */
     public function index(Request $request, CocktailRepository $cocktailRepository, CategoryRepository $categoryRepository): Response
     {
-        $categoryId = null;
+//        $categoryId = null;
+//        $createdAtMin = null;
+//
+//        if ($request->query->count()) {
+//            if ($request->query->has('category')) {
+//                $categoryId = $request->query->get('category');
+//            }
+//            if ($request->query->has('created-at-min')) {
+//                $createdAtMin = $request->query->get('created-at-min');
+//            }
+//        }
+//
+//        $cocktails = $cocktailRepository->search($categoryId, $createdAtMin);
+
+        $category = null;
         $createdAtMin = null;
 
-        if ($request->query->count()) {
-            if ($request->query->has('category')) {
-                $categoryId = $request->query->get('category');
-            }
-            if ($request->query->has('created-at-min')) {
-                $createdAtMin = $request->query->get('created-at-min');
-            }
+        $form = $this->createForm(CocktailSearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $category = $data['category'];
+            $createdAtMin = $data['created_at_min'];
         }
 
-        $cocktails = $cocktailRepository->search($categoryId, $createdAtMin);
-
-        dump($cocktails);
+        $cocktails = $cocktailRepository->search($category, $createdAtMin);
 
         $categories = $categoryRepository->findBy([], ['label' => 'ASC']);
 
         return $this->render('home/index.html.twig', [
             'cocktails' => $cocktails,
-            'categories' => $categories
+            'categories' => $categories,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/CocktailSearchType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CocktailSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'label',
+                'query_builder' => function(CategoryRepository $categoryRepository) {
+                    return $categoryRepository->createQueryBuilder('c')
+                                ->orderBy('c.label', 'ASC');
+                },
+                'label' => 'Catégorie'
+            ])
+            ->add('created_at_min', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Cocktail créé depuis',
+                'input' => 'datetime_immutable'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Filtrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
